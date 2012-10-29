2460a5d6478395dc144a85c6d9b9075ecc2d948b
Scott Ellis <scott@pansenti.com>
2012-10-29 07:17:50 -0400

    Add QMAKEQTE var to yocto-cross-env.sh

--- yocto-cross-env.sh
+++ yocto-cross-env.sh
@@ -5,8 +5,9 @@ fi
 
 export SYSROOTSDIR=${OETMP}/sysroots
 export CROSSTOOLDIR=${SYSROOTSDIR}/`uname -m`-linux/usr/bin
-
 export KERNELDIR=${SYSROOTSDIR}/overo/kernel
 
 PATH=${PATH}:${CROSSTOOLDIR}:${CROSSTOOLDIR}/armv7a-vfp-neon-poky-linux-gnueabi
+
+export QMAKEQTE=/opt/qte/bin/qmake -spec qws/linux-overo-storm-g++
 
