7266dc13f81aa8da2617426aa11f886209b56f36
Scott Ellis <scott@pansenti.com>
2012-11-08 09:42:37 -0500

    Use linux-overo-g++ as the cross platform

--- qt-4.8.3/qt-configure.sh
+++ qt-4.8.3/qt-configure.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#
+# To clear, make confclean
+#
 
 ./configure -prefix /opt/qte \
   -embedded arm \
-  -xplatform qws/linux-overo-storm-g++ \
+  -xplatform qws/linux-overo-g++ \
   -no-glib -no-cups -no-nis -no-largefile -no-accessibility \
   -no-openssl -no-gtkstyle \
   -no-3dnow -no-avx -no-mmx -no-sse -no-sse2 -no-sse3 -no-sse4.1 -no-sse4.2 \
@@ -11,7 +14,12 @@
   -D QT_NO_QWS_CURSOR -D QT_QWS_CLIENTBLIT \
   -depths 16,24,32 \
   -qt-mouse-pc -plugin-mouse-pc \
-  -qt-mouse-tslib -plugin-mouse-tslib \
   -opengl es2 \
   -qt-gfx-linuxfb -plugin-gfx-powervr
+
+
+# Additional flags
+# -qt-mouse-tslib -plugin-mouse-tslib \
+# -nomake examples \
+# -nomake demos \
 
